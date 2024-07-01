c0c5cbdfbad312f1f3f3e21c14fe1a649a99b254
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-01 12:26:53 +0100

    added new pages and new route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use("/example", express.static("example"));
+app.use("/activity", express.static("activity"));
+
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
