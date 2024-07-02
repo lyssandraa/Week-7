26a30f6efbae9565bbb23660293a2114ac53cc32
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-02 11:30:44 +0100

    add initialisation

--- src/server.js
+++ src/server.js
@@ -1,86 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-const fakeData = [
-  { id: 1, title: "book1", author: "author1", genre: "genre1" },
-  { id: 2, title: "book2", author: "author2", genre: "genre2" },
-  { id: 3, title: "book3", author: "author3", genre: "genre3" },
-];
+app.get("/books/getAllBooks", (request, response) => {});
 
-app.get("/books", (request, response) => {
-  console.log(request.path, " :", typeof request.path);
-  response.send("Hello from /books");
-});
+app.post("/books/addBook", (request, response) => {});
 
-app.get("/books/getAllBooks", (request, response) => {
-  const successResponse = {
-    message: "success",
-    books: fakeData,
-  };
+app.put("/books", (request, response) => {});
 
-  response.send(successResponse);
-});
-
-app.post("/books/addBook", (request, response) => {
-  // console.log(request.body);
-
-  fakeData.push(request.body);
-
-  const successResponse = {
-    message: "success",
-    books: fakeData,
-  };
-
-  response.send(successResponse);
-});
-
-app.put("/books", (request, response) => {
-  console.log(request.body);
-  const findBook = (x) => {
-    return x.title === request.body.title;
-  };
-  const index = fakeData.findIndex(findBook);
-
-  if (index === -1) {
-    const failureResponse = {
-      message: `${request.body.title} not found`,
-    };
-    response.send(failureResponse);
-    return;
-  }
-
-  fakeData[index].title = request.body.newTitle;
-
-  const successResponse = {
-    message: "success",
-    books: fakeData[index],
-  };
-
-  response.send(successResponse);
-});
-
-app.delete("/books/deleteBook", (request, response) => {
-  const index = fakeData.findIndex((book) => book.title === request.body.title);
-
-  if (index === -1) {
-    const failureResponse = {
-      message: `${request.body.title} not found`,
-    };
-    response.send(failureResponse);
-    return;
-  }
-
-  fakeData.splice(index, 1);
-  const successResponse = {
-    message: "success",
-    books: fakeData,
-  };
-
-  response.send(successResponse);
-});
+app.delete("/books/deleteBook", (request, response) => {});
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
