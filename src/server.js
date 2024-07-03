38d9ec08dbbf52d87c812393f9d7644aeca7c736
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-07-03 16:02:18 +0100

    added put and delete to update and delete books

--- src/server.js
+++ src/server.js
@@ -33,6 +33,8 @@ const Book = mongoose.model("Book", bookSchema);
 
 // book model ends //
 
+// GET to get all books //
+
 app.get("/books/getAllBooks", async (request, response) => {
   const allBooks = await Book.find({});
   const successResponse = {
@@ -42,6 +44,8 @@ app.get("/books/getAllBooks", async (request, response) => {
 
   response.send(successResponse);
 });
+
+// POST to add a new book //
 
 app.post("/books/addBook", async (request, response) => {
   const book = await Book.create({
@@ -58,20 +62,32 @@ app.post("/books/addBook", async (request, response) => {
   response.send(successResponse);
 });
 
+// PUT to update a book's auhtor by title //
+
 app.put("/books", async (request, response) => {
-  const updatedBook = await Book.updateOne(
+  const updateBook = await Book.updateOne(
     { title: request.body.title },
-    { author: request.body.author }
+    { $set: { author: request.body.author } }
   );
 
   const successResponse = {
     message: "success",
-    updatedBook: updatedBook,
+    updateBook: updateBook,
   };
   response.send(successResponse);
 });
 
-app.delete("/books", (request, response) => {});
+// DEL to delete a book by its title //
+
+app.delete("/books", async (request, response) => {
+  const deleteBook = await Book.deleteOne({ title: request.body.title });
+
+  const successResponse = {
+    message: "success",
+    deleteBook: deleteBook,
+  };
+  response.send(successResponse);
+});
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
